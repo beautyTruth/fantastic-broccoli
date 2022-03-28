6e51c184ba7585f73fe468487c9b6936c436f263
Phil <happy.melodist@gmail.com>
2022-03-28 11:19:27 -0500

    progress through the logging of keyboard events

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+  const canvas = document.querySelector("#icythein");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 720;
+
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener("keydown", function (e) {
+        console.log(e);
+      });
+    }
+  }
+
+  class Player {}
+
+  class Background {}
+
+  class Enemy {}
+
+  function handleEnemies() {}
+
+  function displayStatusText() {}
+
+  const input = new InputHandler();
+
+  function animate() {}
+});
