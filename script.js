69423109f7d5f656cf69fc18e2d5e2ca97d7ebad
Phil <happy.melodist@gmail.com>
2022-03-29 13:38:53 -0500

    complete project through the setting up of keydown and and keyup event listeners for the arrow down

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ window.addEventListener("load", function () {
   class InputHandler {
     constructor() {
       this.keys = [];
-      window.addEventListener("keydown", function (e) {
-        console.log(e);
+      window.addEventListener("keydown", (e) => {
+        console.log(e.key);
+        if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
+          this.keys.push(e.key);
+        }
+        console.log(e.key, this.keys);
+      });
+      window.addEventListener("keyup", (e) => {
+        if (e.key === "ArrowDown") {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+        console.log(e.key, this.keys);
       });
     }
   }
