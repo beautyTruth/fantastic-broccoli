ce748d681639364b12c9a5a226053a0bccb0b783
Phil <happy.melodist@gmail.com>
2022-04-12 21:44:52 -0500

    complete keyboard inputHandler class and instantiate it

--- script.js
+++ script.js
@@ -8,17 +8,25 @@ window.addEventListener("load", function () {
     constructor() {
       this.keys = [];
       window.addEventListener("keydown", (e) => {
-        console.log(e.key);
-        if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
+        if (
+          (e.key === "ArrowDown" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowLeft" ||
+            e.key === "ArrowRight") &&
+          this.keys.indexOf(e.key) === -1
+        ) {
           this.keys.push(e.key);
         }
-        console.log(e.key, this.keys);
       });
       window.addEventListener("keyup", (e) => {
-        if (e.key === "ArrowDown") {
+        if (
+          e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight"
+        ) {
           this.keys.splice(this.keys.indexOf(e.key), 1);
         }
-        console.log(e.key, this.keys);
       });
     }
   }
