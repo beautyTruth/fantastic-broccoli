ae3d9f9b4cbc91e81e416e3fe9c0724d9e7d87b6
Phil <happy.melodist@gmail.com>
2022-04-12 22:26:23 -0500

    complete the project through the calling of the built-in draw function on the context

--- script.js
+++ script.js
@@ -31,7 +31,20 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Player {}
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = 0;
+    }
+    draw(context) {
+      context.fillStyle = "cornflowerblue";
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
   class Background {}
 
@@ -42,6 +55,8 @@ window.addEventListener("load", function () {
   function displayStatusText() {}
 
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
 
   function animate() {}
 });
