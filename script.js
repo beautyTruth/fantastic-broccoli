1d0bd95af47369b328773cba41a0fa621cea23e8
Phil <happy.melodist@gmail.com>
2022-04-14 08:43:47 -0500

    complete project through the addition of the right arrow move

--- script.js
+++ script.js
@@ -39,14 +39,34 @@ window.addEventListener("load", function () {
       this.height = 200;
       this.x = 0;
       this.y = this.gameHeight - this.height;
+      this.image = document.getElementById("playerImage");
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
     }
     draw(context) {
       context.fillStyle = "cornflowerblue";
       context.fillRect(this.x, this.y, this.width, this.height);
-      context.drawImage();
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
     }
-    update() {
-      this.x++;
+    update(input) {
+      // horizontal movement
+      this.x += this.speed;
+      if (input.keys.indexOf("ArrowRight") > -1) {
+        this.speed = 5;
+      } else {
+        this.speed = 0;
+      }
     }
   }
 
@@ -65,7 +85,7 @@ window.addEventListener("load", function () {
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw(ctx);
-    player.update();
+    player.update(input);
     requestAnimationFrame(animate);
   }
   animate();
